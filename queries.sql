5ea83fa52bec2fd4a6dff1398f79b092ad6c24ea
Dream Higher Visions <dreamhighervisions@Dreams-MacBook-Air.local>
2020-02-03 02:52:31 -0600

    Assignment 4 complete

--- queries.sql
+++ queries.sql
@@ -1,7 +1,20 @@
 ## Part 1: Test it with SQL
+    DESCRIBE job; returns:
+        name:varchar(125)
+        employer_Id:int(11)
+        jobs_id:int(11)
 
 ## Part 2: Test it with SQL
+    SELECT name
+    FROM techJobs.employer
+    where location = "St. Louis City.";
 
 ## Part 3: Test it with SQL
+    DROP TABLE `techJobs`.`job`;
 
 ## Part 4: Test it with SQL
+    SELECT Distinct name
+FROM techJobs.skill
+left join techJobs.job_skills on techJobs.skill.id = techJobs.job_skills.skills_id
+where techJobs.job_skills.skills_id is not null
+order by name asc;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,45 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(min = 3, max = 50, message = "Location required, must have between 3 and 50 characters")
+    private String location;
 
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+    }
+
+    public Employer(@NotNull @Size(min = 3, max = 50, message = "Location required, must have between 3 and 50 characters") String location, List<Job> jobs) {
+        this.location = location;
+        this.jobs = jobs;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
